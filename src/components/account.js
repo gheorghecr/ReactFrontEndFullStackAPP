7d778528aef5ee492ca996744fb627af3d99ee45
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-13 17:08:05 +0000

    Added just an account page

---
+++ src/components/account.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Account() {
+  return (
+    <>
+      <h1>Post ID:</h1>
+      <p>This is where account article posts can be displayed</p>
+    </>
+  );
+}
+
+export default Account;
