8e86aabc54673f0d530d9708e607d9423d4a212b
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-13 17:08:23 +0000

    Adding APP.js first screnns

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './components/nav';
+import Account from './components/account';
+import Register from './components/register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserContext from './contexts/user';
+
+import { Layout } from 'antd';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const { Header, Content, Footer } = Layout;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { loggedIn: false }
+    }
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  login(user) {
+    console.log("User is now being set on the context");
+    user.loggedIn = true;
+    this.setState({ user: user });
+  }
+
+  logout() {
+    console.log("Removing user from the app context");
+    this.setState({ user: { loggedIn: false } });
+  }
+
+  render() {
+    const context = {
+      user: this.state.user,
+      login: this.login,
+      logout: this.logout
+    };
+
+    return (
+      <UserContext.Provider value={context}>
+        <Router>
+          <Layout className="layout">
+            <Header>
+             <Nav /> 
+            </Header>
+
+            <Content>
+              <Switch>
+                
+                <Route path="/account" children={<Account />} />
+                <Route path="/register" children={<Register />} />
+                 {/* <Route path="/login" children={<Login />} />
+                <Route path="/post/:id" children={<Post />} />
+                <Route path="/" children={<Home />} exact />
+                */}
+              </Switch>
+            </Content>
+
+            <Footer style={{ textAlign: 'center' }}>Created for 304CEM Assignment by Gheorghe Craciun </Footer>
+          </Layout>
+        </Router>
+      </UserContext.Provider>
+    );
+  }
 }
 
 export default App;
