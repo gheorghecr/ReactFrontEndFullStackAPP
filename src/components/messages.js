3cf1beec0eec42a28963aef6a10f7f763c453388
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-25 16:47:46 +0000

    Changing the messaging part of the application

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import UserContext from '../contexts/user';
-import { Col, Row, Affix, Button, Modal, Tabs } from 'antd';
+import { Col, Row, Button, Avatar, } from 'antd';
 // import { status, json } from '../utilities/requestHandlers';
 import { MessageOutlined } from '@ant-design/icons';
 
-const { TabPane } = Tabs;
 
+/**
+ * Component that manages the messages. It has the list of messages.
+ * And then as well the opened conversation.
+ * 
+ */
 class Messages extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
-      visible: false,
+      
     }
   }
 
@@ -25,58 +28,14 @@ class Messages extends React.Component {
   }
 
   static contextType = UserContext;
-
-
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
-
-  callback(key) {
-    console.log(key);
-  }
+  
 
   render() {
-    console.log(this.state.height)
+
+   
+
     return (
       <>
-        <Affix offsetTop={90} style={{ marginLeft: 40 }} icon={<MessageOutlined />} onChange={(affixed) => console.log(affixed)}>
-          <Button type="primary" shape="round" size='large' onClick={this.showModal} > Messages </Button>
-        </Affix>
-
-        <Modal
-          title="Messages"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-          okButtonProps={{ disabled: false }}
-          cancelButtonProps={{ disabled: false }}
-        >
-          <Tabs defaultActiveKey="1" >
-            <TabPane tab="List of Conversations" key="1">
-              Content of Tab Pane 1
-            </TabPane>
-            <TabPane tab="Chat" key="2">
-              Content of Tab Pane 2
-            </TabPane>
-          </Tabs>
-        </Modal>
-
       </>
     );
   }
